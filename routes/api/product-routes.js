c404d970371386ba23dee0c56d79ae4926c34174
vantam8300 <vantam8300@gmail.com>
2022-12-11 00:28:53 -0800

    update message in product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -26,7 +26,7 @@ router.get('/:id', async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: 'No category found with this id!' });
+      res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
 
@@ -120,7 +120,7 @@ router.delete('/:id',async (req, res) => {
     });
 
     if (!productData) {
-      res.status(404).json({ message: 'No Category found with this id!' });
+      res.status(404).json({ message: 'No product found with this id!' });
       return;
     }
 
